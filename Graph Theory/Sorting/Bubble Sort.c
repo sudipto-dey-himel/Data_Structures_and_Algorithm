b4bfe02e87cde145135403d80e0137e00b510d0d
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-07-22 12:03:07 +0600

    Create Bubble Sort.c

---
+++ Graph Theory/Sorting/Bubble Sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void sort(int arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-1-i; j++) {
+            if(arr[j]>arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
+void print(int arr[], int n) {
+    for(int i=0; i<n; i++)
+    printf("%d  ", arr[i]);
+}
+int main() {
+    int n;
+    printf("Enter the number of elements :: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements :: ");
+    for(int i=0; i<n; i++) 
+    scanf("%d", &arr[i]);
+    sort(arr, n);
+    printf("Sorted Array :: ");
+    print(arr, n);
+}
