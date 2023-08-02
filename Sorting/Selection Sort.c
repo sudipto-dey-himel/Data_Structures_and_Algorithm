6113515e4be36be1d5ebe544b2e9d5e59a836314
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-08-02 09:52:41 +0600

    Update Selection Sort.c

--- Sorting/Selection Sort.c
+++ Sorting/Selection Sort.c
@@ -5,7 +5,7 @@ void swap(int *a, int *b) {
     *b = temp;
 }
 void sort(int arr[], int n) {
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n-1; i++) {
         if(arr[i]>arr[i+1])
         swap(&arr[i], &arr[i+1]);
     }
