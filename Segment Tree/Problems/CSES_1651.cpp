e990b43e67650f1e41ccd35732ba17c1f51f7fda
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-05-21 10:21:33 +0600

    Update CSES_1651.cpp

--- Segment Tree/Problems/CSES_1651.cpp
+++ Segment Tree/Problems/CSES_1651.cpp
@@ -85,8 +85,13 @@ void solve() {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-    solve();
+    // ll t; cin >> t;
+    // while(t--) {
+        solve();
+    // }
 
     return 0;
 }
